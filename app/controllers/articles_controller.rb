33d6176957c008d934c1fa803a9ef568f5774d67
Kang Chen <kangchen.us@gmail.com>
2018-11-17 12:21:43 -0800

    add partials and delete function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all 
+    @articles = Article.all
   end
 
 # creating an article
@@ -21,11 +22,9 @@ class ArticlesController < ApplicationController
 
 # updating an article
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -34,11 +33,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
   def show
-    @article = Article.find(params[:id])
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
