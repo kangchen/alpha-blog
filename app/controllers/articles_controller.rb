6a2d4589fb6431a8ce0be352c88be2c51ffeb7a8
Kang Chen <kangchen.us@gmail.com>
2018-11-21 02:11:14 -0800

    app can't started, debugging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # debugger
     @article = Article.new(article_params)
     @article.user = User.find(rand(1..5))
     if @article.save
