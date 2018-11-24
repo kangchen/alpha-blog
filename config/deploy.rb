d8cc82d69fce601c45350c0dbb2509217c7b4e56
Kang Chen <kangchen.us@gmail.com>
2018-11-24 11:06:12 -0800

    deploy.rb changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:kangchen/alpha-blog.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/kangchen/Sites/alpha-blog/'
+set :deploy_to, '/home/ubuntu/Sites/alpha-blog/'
 
 set :linked_files, %w{config/master.key}
 
