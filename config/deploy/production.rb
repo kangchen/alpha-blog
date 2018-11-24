33823f0dad84fac59071f5936f4222e7230e3070
Kang Chen <kangchen.us@gmail.com>
2018-11-23 23:28:26 -0800

    deploy to azure virtual machine

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '34.217.192.116', user: 'ubuntu', roles: %w{app web}
+server '104.211.31.90', user: 'ubuntu', roles: %w{app web}
 
 # role-based syntax
 # ==================
