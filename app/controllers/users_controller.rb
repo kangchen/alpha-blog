a601b32ae162335219c0f875935c4cdda9d7c5d4
Kang Chen <kangchen.us@gmail.com>
2018-12-06 09:43:41 -0800

    styling dragdown menu and more animation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def index
     # @users = User.all
     @users = User.paginate(page: params[:page], :per_page => PER_PAGE).order('username ASC')
+    expires_in 3.minutes, :public => true
   end
 
   def new
