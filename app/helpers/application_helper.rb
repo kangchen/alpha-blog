f795926f15c2f9f6cc7211409e4cedcf0228f4e3
Kang Chen <kangchen.us@gmail.com>
2018-12-09 01:22:52 -0800

    application_helper error fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,15 +5,15 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.username, class: "circle")
   end
-  # 
-  # def will_paginate(collection_or_options = nil, options = {})
-  #   if collection_or_options.is_a? Hash
-  #     options, collection_or_options = collection_or_options, nil
-  #   end
-  #   unless options[:renderer]
-  #     options = options.merge renderer: WillPaginate::ActionView::BootstrapLinkRenderer
-  #   end
-  #   super *[collection_or_options, options].compact
-  # end
+  #
+  def will_paginate(collection_or_options = nil, options = {})
+    if collection_or_options.is_a? Hash
+      options, collection_or_options = collection_or_options, nil
+    end
+    unless options[:renderer]
+      options = options.merge renderer: WillPaginate::ActionView::BootstrapLinkRenderer
+    end
+    super *[collection_or_options, options].compact
+  end
 
 end
