9b87f91a1073e80d6884357e835ea09f084e7286
Kang Chen <kangchen.us@gmail.com>
2018-12-21 08:40:49 -0800

    .

--- app/helpers/avators_helper.rb
+++ app/helpers/avators_helper.rb
@@ -3,7 +3,8 @@ module AvatorsHelper
   def get_avator_image(user, size)
     avator_image = Avator.where("uid = ?", user.id)
     if avator_image.count > 0
-      return image_tag(avator_image[0].img_resize(size), class: 'profile')
+      # return image_tag(avator_image[0].img_resize(size), class: 'profile')
+      return image_tag(avator_image[0].image, class: 'profile')
     else
       return image_tag('https://picsum.photos/200/300?image='+((user.id % 1000) + LOREM_PIC).to_s, class: 'picsum')
     end
