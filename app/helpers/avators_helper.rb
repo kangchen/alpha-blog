d09099d93c324a4ae0b80615650fb11538e8623d
Kang Chen <kangchen.us@gmail.com>
2018-12-21 09:39:50 -0800

    .

--- app/helpers/avators_helper.rb
+++ app/helpers/avators_helper.rb
@@ -6,8 +6,8 @@ module AvatorsHelper
       return image_tag(avator_image[0].img_resize(size), class: 'profile')
       # return image_tag(avator_image[0].image, class: 'profile', style: 'width:36px; height:36px')
     else
-      # style = 'width:'+size.to_s+'px; height:'+size.to_s+'px;'
-      return image_tag('https://picsum.photos/200/300?image='+((user.id % 1000) + LOREM_PIC).to_s, class: 'profile')
+      style = 'width:'+size.to_s+'px; height:'+size.to_s+'px;'
+      return image_tag('https://picsum.photos/200/300?image='+((user.id % 1000) + LOREM_PIC).to_s, class: 'profile', style: style)
     end
   end
 
