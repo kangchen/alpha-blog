56922607d0487e073a1b89e0ea1ea5a38d25faee
Kang Chen <kangchen.us@gmail.com>
2018-12-21 15:20:31 -0800

    make a link to active storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,9 @@ set :linked_files, %w{config/master.key}
 
 append :linked_files, "config/database.yml"
 
+# Active Storage
+append :linked_dirs, "storage"
+
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
