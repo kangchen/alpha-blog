6f0ec3e9700314870acefd2711101ae4ea67799f
Kang Chen <kangchen.us@gmail.com>
2018-12-21 15:30:56 -0800

    make a linked dirs storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,11 +29,8 @@ set :linked_files, %w{config/master.key}
 
 append :linked_files, "config/database.yml"
 
-# Active Storage
-append :linked_dirs, "storage"
-
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "storage", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
